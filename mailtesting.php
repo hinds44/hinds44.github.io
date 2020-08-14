39c4328b049c4dae17fdc4b5df2c3fc89d090d88
Liam Hinds <liam.hinds@gmail.com>
2020-08-14 22:38:40 +0100

    mailtesting added to...
    ... test mail

---
+++ mailtesting.php
@@ -0,0 +1,17 @@
+<?PHP
+$sender = 'liam.hinds@gmail.com';
+$recipient = 'liam.hinds@gmail.com';
+
+$subject = "php mail test";
+$message = "php test message";
+$headers = 'From:' . $sender;
+
+if (mail($recipient, $subject, $message, $headers))
+{
+    echo "Message accepted";
+}
+else
+{
+    echo "Error: Message not accepted";
+}
+?>
